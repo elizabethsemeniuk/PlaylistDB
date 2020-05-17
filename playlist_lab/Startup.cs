bba6c280e7bdb9ef49a64e69b3796680a5df3afa
elizabethsemeniuk <lizznight@gmail.com>
2020-05-17 03:39:37 +0300

    Changed appsettings.json, Startup.cs
    Строка подключения, изменение метода ConfigureService

--- playlist_lab/Startup.cs
+++ playlist_lab/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using playlist_lab.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace playlist_lab
 {
@@ -23,6 +25,8 @@ namespace playlist_lab
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<DBPlaylistContext>(options => options.UseSqlServer(connection));
             services.AddControllersWithViews();
         }
 
